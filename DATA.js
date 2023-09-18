e58856a12107c69c09002c547bf1ca87825ec047
thecoder.live <82015917+thecoderlive@users.noreply.github.com>
2023-09-18 18:53:34 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,145 @@
+export const item = {
+    "id": "19230c23-08ab-4ad7-aa99-949cc8fde317",
+    "home_feed": [
+        {
+            "id": "59e3be9d-e95a-4489-9d8c-c3b674cec264",
+            "artist_name": "well",
+            "home_carousel": [
+                {
+                    "id": "f981dee0-d146-4660-8c04-ec91807b946b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/312.png"
+                },
+                {
+                    "id": "ad38c072-3117-4237-81b0-fe9dee7251b0",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/77.png"
+                },
+                {
+                    "id": "5cc22c33-88cd-496c-8576-dc9a692cb2e2",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/338.png"
+                },
+                {
+                    "id": "e1953cf7-9c0b-4de2-97eb-f9188db6430e",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/127.png"
+                },
+                {
+                    "id": "a6b7317d-cd2f-4f99-8311-b9357f788acc",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/305.png"
+                }
+            ],
+            "art_caption": "training",
+            "art_description": "Film detail magazine hundred just."
+        },
+        {
+            "id": "1fa99899-42d7-48d5-9add-bcc6408833a1",
+            "artist_name": "floor",
+            "home_carousel": [
+                {
+                    "id": "fcf662db-e4ae-4d19-b25b-01a06a679275",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/331.png"
+                },
+                {
+                    "id": "238cdbde-ba04-4722-8c7d-b6e4b5a95b9f",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/86.png"
+                },
+                {
+                    "id": "2c6671b8-79a1-4646-9f6c-2436916e80dc",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/348.png"
+                },
+                {
+                    "id": "0a2aba38-7bc2-4fa3-a02a-73203f3d7d43",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/281.png"
+                },
+                {
+                    "id": "5e2ec7d2-fcd4-4489-97c8-a00f7a140972",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/165.png"
+                }
+            ],
+            "art_caption": "traditional",
+            "art_description": "Tonight while sit test school out. Suddenly now bar about. Push top to total country still huge."
+        },
+        {
+            "id": "79226a76-6553-411d-b60d-515cf884ab5e",
+            "artist_name": "their",
+            "home_carousel": [
+                {
+                    "id": "eb9a0e0d-8429-43f0-ae4d-59f7d26ebe77",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/13.png"
+                },
+                {
+                    "id": "04ae3518-b8cf-449b-9ac3-14fca6dc961a",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/125.png"
+                },
+                {
+                    "id": "5867be90-d448-4a36-bf98-61c254ed6f00",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/64.png"
+                },
+                {
+                    "id": "7d0deca5-ec97-418d-aa51-eb5c318ae73c",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/40.png"
+                },
+                {
+                    "id": "410ddaf6-161d-4bf3-ba24-4bb95b645e58",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/90.png"
+                }
+            ],
+            "art_caption": "sign",
+            "art_description": "Try talk federal open. Behind common unit collection everyone identify federal with. So artist somebody affect that only safe."
+        },
+        {
+            "id": "abcff81b-e302-44cf-8322-817fe0c1438c",
+            "artist_name": "stand",
+            "home_carousel": [
+                {
+                    "id": "3d4b3577-baf5-44e2-a874-7c9dc5323ab6",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/323.png"
+                },
+                {
+                    "id": "ae256b5e-6509-47c9-b5eb-fac5ad783ae1",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/234.png"
+                },
+                {
+                    "id": "680b54b7-3682-4aeb-ad46-fd9c7329568a",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/94.png"
+                },
+                {
+                    "id": "a87d4aaa-082c-4a38-8de1-e758cdc38f33",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/232.png"
+                },
+                {
+                    "id": "eed7cc41-61e1-4c47-8a02-2c207171a9c9",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/31.png"
+                }
+            ],
+            "art_caption": "suddenly",
+            "art_description": "Their board result eight upon. Him audience minute write easy those. Hold receive million similar between me. Our mission face happen check growth paper purpose."
+        },
+        {
+            "id": "2eb03cf0-d3a6-4b32-bd1d-a89e2146133e",
+            "artist_name": "half",
+            "home_carousel": [
+                {
+                    "id": "ecc5b9d9-a8ec-4444-853b-d5b523ce43b7",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/102.png"
+                },
+                {
+                    "id": "a5477ee7-ebfe-4a9d-947d-e2a143eceabe",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/350.png"
+                },
+                {
+                    "id": "10d86e58-4b23-4b25-b3d4-0d027be48008",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/52.png"
+                },
+                {
+                    "id": "c1a6900d-1161-435a-9aea-db28f68658cd",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/335.png"
+                },
+                {
+                    "id": "e0ffb947-ae47-4391-ba99-9ce3da4410e9",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/137.png"
+                }
+            ],
+            "art_caption": "sister",
+            "art_description": "Before run suffer best mouth live political."
+        }
+    ]
+}
